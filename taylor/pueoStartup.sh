5dc66bfb97fd547b5880676360f1b1009bb4ef12
Payton <lintonpayton99@gmail.com>
2025-06-28 15:54:14 -0500

    pueoStartup.sh now runs through mtsAdvance.py. Does not handle errors currently. Probably need to fork repo and will need to edit other scripts to give error codes and expand state machine

--- taylor/pueoStartup.sh
+++ taylor/pueoStartup.sh
@@ -3,14 +3,32 @@
 if [ $# -eq 0 ]
     then 
         echo "Starting up all TURFIOs"
-        /home/pueo/taylor/ppython /home/pueo/startup/turfManualStartup
+        /home/pueo/taylor/ppython /home/pueo/startup/turfManualStartup.py
+        /home/puep/taylor/ppython /home/pueo/startup/surfStartup.py --enable --tio 0 --slots 0,1,2,3,4,5,6
+        /home/puep/taylor/ppython /home/pueo/startup/surfStartup.py --enable --tio 1 --slots 0,1,2,3,4,5,6
+        /home/puep/taylor/ppython /home/pueo/startup/surfStartup.py --enable --tio 2 --slots 0,1,2,3,4,5
+        /home/puep/taylor/ppython /home/pueo/startup/surfStartup.py --enable --tio 3 --slots 0,1,2,3,4,5
+        /home/puep/taylor/ppython /home/pueo/startup/mtsAdcance.py
 fi
 
 while test $# -gt 0; do
     case $1 in 
         -t|--tio)
             shift
-            /home/pueo/taylor/ppython /home/pueo/startup/turfManualStartup --turfio $1
+            /home/pueo/taylor/ppython /home/pueo/startup/turfManualStartup.py --turfio $1
+            if $1 -eq 0; then
+                /home/puep/taylor/ppython /home/pueo/startup/surfStartup.py --enable --tio 0 --slots 0,1,2,3,4,5,6
+                /home/puep/taylor/ppython /home/pueo/startup/mtsAdcance.py --tio 0
+            else if $1 -eq 1; then
+                /home/puep/taylor/ppython /home/pueo/startup/surfStartup.py --enable --tio 1 --slots 0,1,2,3,4,5,6
+                /home/puep/taylor/ppython /home/pueo/startup/mtsAdcance.py --tio 1
+            else if $1 -eq 2; then
+                /home/puep/taylor/ppython /home/pueo/startup/surfStartup.py --enable --tio 2 --slots 0,1,2,3,4,5
+                /home/puep/taylor/ppython /home/pueo/startup/mtsAdcance.py --tio 2
+            else if $1 -eq 3; then
+                /home/puep/taylor/ppython /home/pueo/startup/surfStartup.py --enable --tio 3 --slots 0,1,2,3,4,5
+                /home/puep/taylor/ppython /home/pueo/startup/mtsAdcance.py --tio 3
             shift;;
+
     esac
 done
