59d493876353579166e69bfc0786657c6b0ce7d7
Taylor Coakley <coakley.64@osu.edu>
2025-06-30 12:15:12 -0400

    function to check the linkstat

--- taylor/autoStartup/checkAuroraBridge.py
+++ taylor/autoStartup/checkAuroraBridge.py
@@ -2,3 +2,18 @@ from pueo.turf import PueoTURF
 from pueo.turfio import PueoTURFIO
 from pueo.surf import PueoSURF
 
+dev = PueoTURF()
+
+def bridgeCheck(): 
+    dev = PueoTURF()
+    down = []
+    for i in range(0,4): 
+        val = dev.aurora.linkstat(i)
+        if val != 831: 
+            down.append(i)
+        
+    if len(down) == 0: 
+        return [4]
+    else: 
+        return down
+    
