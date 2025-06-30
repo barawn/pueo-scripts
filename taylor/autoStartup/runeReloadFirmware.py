78e84a25dc666f4732d2b29dfb750a6afc1d5423
Taylor Coakley <coakley.64@osu.edu>
2025-06-30 12:34:15 -0400

    script to reset an aurora link

---
+++ taylor/autoStartup/runeReloadFirmware.py
@@ -0,0 +1,14 @@
+from HskSerial import HskPacket
+
+def runeReloadFirmware(hsk, port):
+    # map of all turfios to ports for the turf
+    tios = [ (0, 0x58), 
+            (1, 0x50), 
+            (2, 0x40), 
+            (3, 0x48) ] 
+    
+    # if port is valid, reload firmware 
+    for i in range(len(port)): 
+        for tio in tios: 
+            if port[i] == tio[0]: 
+                hsk.send(HskPacket(tio[1], 'eReloadFirmware', data = [0,0,0,0]))
