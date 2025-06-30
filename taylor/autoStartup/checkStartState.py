6e60f79718e86274b2ff2f3c8d00b75fb338d74a
Taylor Coakley <coakley.64@osu.edu>
2025-06-30 17:05:44 -0400

    script ot check the start state

---
+++ taylor/autoStartup/checkStartState.py
@@ -0,0 +1,72 @@
+# from HskSerial import HskEthernet, HskPacket
+import time
+from turfsurfAddrs import addr
+
+def checkStartState(hsk): 
+
+    tios, surfs = addr()
+    failed = []
+    for tio in tios: 
+        # hsk.send(HskPacket(tio[1], 'eEnable', data = [0x40, 0x40]))
+        # pkt = hsk.receive()
+        idx = tios.index(tio)
+        for surf in surfs[idx]:
+            pkt = 0 
+            hsk.send(HskPacket(surf[1], 'eStartState'))
+            try:
+                pkt = hsk.receive().data
+                if pkt is None:
+                    failed.append((tio[1], surf[0]))
+                    print('failed to receive a response')
+            except: 
+                continue 
+    
+
+
+
+    """hsk.send(HskPacket(0x40, 'eEnable', [0x40, 0x40]))
+    pkt = hsk.receive()
+    hsk.send(HskPacket(0x48, 'eEnable', [0x40, 0x40]))
+    pkt = hsk.receive()
+    hsk.send(HskPacket(0x50, 'eEnable', [0x40, 0x40]))
+    pkt = hsk.receive()
+    hsk.send(HskPacket(0x58, 'eEnable', [0x40, 0x40]))
+    pkt = hsk.receive()
+
+    print("Are you Ready kids?")"""
+"""
+    surfs = [ (0, 0x97),
+            (1, 0xa0),
+            (2, 0x99),
+            (3, 0x8d),
+            (4, 0x9d),
+            (5, 0x94),
+            (6, 0x8a),
+            (0, 0x8c),
+            (1, 0x95),
+            (2, 0x9f),
+            (3, 0x9a),
+            (4, 0x87),
+            (5, 0x85), 
+            (6, 0x9c),
+            (0, 0x89),
+            (1, 0x88),
+            (2, 0x9e),
+            (3, 0x8b),
+            (4, 0xa1),
+            (5, 0x98),
+            (0, 0x93),
+            (1, 0x9b),
+            (2, 0x96),
+            (3, 0x8e),
+            (4, 0x90),
+            (5, 0x92)]
+
+    for ii in range(len(surfs)):
+        hsk.send(HskPacket(surfs[ii][1], 'eStartState'))
+        rm = hsk.receive()
+        print(rm.pretty())
+
+    print('Aye Aye, Captain!')"""
+
+checkStartState(1)
