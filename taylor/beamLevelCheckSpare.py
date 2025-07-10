b5a6634349ca537b4bd687875c24d41242791b62
tmcoakley <coakley.64@osu.edu>
2025-07-10 14:56:18 +0000

    rf trigger testing scripts

---
+++ taylor/beamLevelCheckSpare.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from HskSerial import HskEthernet, HskPacket
+import argparse
+import pickle
+from pueo.turf import PueoTURF
+from pueo.turfio import PueoTURFIO
+from pueo.surf import PueoSURF
+from EventTester import EventServer
+import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--threshold', type = int)
+args = parser.parse_args()
+
+
+dev = PueoTURF()
+
+tio1 = PueoTURFIO((dev, 0), 'TURFGTP')
+
+surf1 = PueoSURF((tio1, 5), 'TURFIO')
+
+print(f'SURF 5 Threshold: {surf1.levelone.read(0x0800)}')
+print(dev.trig.scaler.scalers(verbose = True))
--- taylor/rftriggerSpare.py
+++ taylor/rftriggerSpare.py
@@ -20,21 +20,15 @@ dev = PueoTURF()
 es = EventServer()
 
 tio1 = PueoTURFIO((dev, 0), 'TURFGTP')
-tio2 = PueoTURFIO((dev, 3), 'TURFGTP')
 
 surf1 = PueoSURF((tio1, 5), 'TURFIO')
-surf2 = PueoSURF((tio2, 5), 'TURFIO')
 
-if surf1.trig_clock_en != 1 or surf2.trig_clock_en != 1: 
+if surf1.trig_clock_en != 1 : 
     print("Yo, the RF stuff ain't set up right")
-    sys.exit(1)
+    sys.exit(1) 
 
-surf1.levelone.write(0x1008, 0x30000)
-surf1.levelone.write(0x100C, 0xffffffff)
-surf2.levelone.write(0x1008, 0x30000)
-surf2.levelone.write(0x100C, 0xffffffff)
-
-dev.trig.mask = 201326559
+dev.event.mask = 0b1110
+dev.trig.mask = 268435423
 
 es.open()
 dev.trig.runcmd(dev.trig.RUNCMD_RESET)
