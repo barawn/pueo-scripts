56499ee250232c0420446b0483eecc21e2968856
Taylor Coakley <coakley.64@osu.edu>
2025-07-25 22:09:20 -0400

    adding separate script for rf trigger with all crate

---
+++ taylor/beamLevelCheckAll.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from HskSerial import HskEthernet, HskPacket
+import argparse
+import pickle
+from pueo.turf import PueoTURF
+from pueo.turfio import PueoTURFIO
+from pueo.surf import PueoSURF
+from EventTester import EventServer
+import time
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--tio", type=int)
+parser.add_argument("--slots", type=str, default="0,1,2,3,4,5,6")
+
+args = parser.parse_args()
+
+slotList = list(map(int,args.slots.split(',')))
+
+
+dev = PueoTURF(None, 'Ethernet')
+
+tio = PueoTURFIO((dev, args.tio), 'TURFGTP')
+
+for slot in slotList: 
+    surf = PueoSURF((tio, slot), 'TURFIO')
+    
+    print(f'SURF 5 Threshold: {surf.levelone.read(0x0800)}')
+    print(dev.trig.scaler.scalers(verbose = True))
