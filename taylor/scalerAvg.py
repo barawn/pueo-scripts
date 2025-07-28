3b5571f69d62520cacf0e97e9ecdd2820bff88bb
Taylor Coakley <coakley.64@osu.edu>
2025-07-28 10:54:52 -0400

    averaging the scalers

---
+++ taylor/scalerAvg.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from HskSerial import HskEthernet, HskPacket
+import argparse
+import pickle
+from pueo.turf import PueoTURF
+from pueo.turfio import PueoTURFIO
+from pueo.surf import PueoSURF
+from EventTester import EventServer
+import time
+import numpy as np
+
+dev = PueoTURF(None, 'Ethernet')
+
+# turfio 3 slots 5 and 6 
+slot5 = []
+slot6 = []
+for i in range(0, 60): 
+    slot5.append(dev.trig.scaler.read(28 * 4))
+    slot6.append(dev.trig.scaler.read(29 * 4))
+    time.sleep(1)
+
+print(f'Average scaler SURF 16: {np.mean(slot5)}')
+print(f'Average scaler SURF 18: {np.mean(slot6)}')
