c9e7cbe6aacdf1bcd8d8977bd254996420f47752
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2025-07-30 16:11:04 +0000

    jims agc scripts

--- taylor/viewBeams4.py
+++ taylor/viewBeams4.py
@@ -4,7 +4,7 @@ from pueo.surf import PueoSURF
 
 dev =PueoTURF()
 tio=PueoTURFIO((dev,3),'TURFGTP')
-surf=PueoSURF((tio,5),'TURFIO')
+surf=PueoSURF((tio,4),'TURFIO')
 for i in range(46):
     rate=surf.levelone.read(0x400+4*i)
     print(f"{rate:7d}"," ",end="")
