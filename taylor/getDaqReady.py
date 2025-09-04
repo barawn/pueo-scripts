e87104093d4d6e02f333458da05038ab1de817d0
Taylor Coakley <coakley.64@osu.edu>
2025-09-04 11:07:18 -0400

    im trying something

---
+++ taylor/getDaqReady.py
@@ -0,0 +1,28 @@
+import sys
+import subprocess
+cmd = 'taylor/ppython startup/turfManualStartup.py '
+
+p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+out, err = p.communicate() 
+result = out.split('\n')
+for lin in result:
+    if not lin.startswith('#'):
+        print(lin)
+
+
+"""
+taylor/ppython startup/turfManualStartup.py 
+taylor/ppython startup/surfStartup.py --tio 3 --slots 0,1,2,3,4,5 --enable
+taylor/ppython startup/surfStartup.py --tio 2 --slots 0,1,2,3,4,5 --enable
+taylor/ppython startup/surfStartup.py --tio 1 --slots 0,1,2,3,4,5,6 --enable
+taylor/ppython startup/surfStartup.py --tio 0 --slots 0,1,2,3,4,5,6 --enable
+taylor/ppython taylor/mtsAdvance.py --tio 3 
+taylor/ppython taylor/mtsAdvance.py --tio 2
+taylor/ppython taylor/mtsAdvance.py --tio 1
+taylor/ppython taylor/mtsAdvance.py --tio 0
+taylor/ppython taylor/runEvent.py --stop 1 --filename softCheck
+taylor/ppython taylor/rfStartAll.py --tio 3 --slots 4,5
+taylor/ppython taylor/thresholdRFAll.py --threshold 19000 --tio 3 --slots 4,5 --freeze
+taylor/ppython taylor/beamLevelCheckAll.py --tio 3 --slots 4,5
+taylor/ppython taylor/rftrigger.py --stop 1 --filename rfCheck
+"""
