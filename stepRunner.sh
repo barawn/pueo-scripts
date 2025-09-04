482f9b42aa4795c366afe2ca2c5d0f038a70220f
Taylor Coakley <coakley.64@osu.edu>
2025-09-04 11:51:40 -0400

    im silly

--- stepRunner.sh
+++ stepRunner.sh
@@ -16,7 +16,9 @@ while IFS= read -r line || [[ -n "$line" ]]; do
             eval "$line"
             status=$?
             if echo "$output" | grep -q "TURFIO bridge error"; then
-                echo -e "\033[1;31m❌ Detected TURFIO bridge error. Consider rebooting the TURF.\033[0m"
+                echo -e "\033[1;31 Detected TURFIO bridge error. Consider rebooting the TURF.\033[0m"
+            elif echo "$output" | grep -q "GTP link"; then
+                echo -e "\033[1;31m Detected GTP link error. Consider rebooting the TURF.\033[0m"
             elif [ $status -eq 0 ]; then
                 echo -e "\033[1;32m Success\033[0m"
             else
