ceedc84eed6b98a1d52adef446e314c37167daae
Taylor Coakley <coakley.64@osu.edu>
2025-09-19 10:33:22 -0400

    break missing

--- getDAQReady.sh
+++ getDAQReady.sh
@@ -114,13 +114,14 @@ while IFS= read -r line || [[ -n "$line" ]]; do
                 elif echo "$output" | grep -q "RX clock off"; then
                     echo -e "\033[1;31No clock alignment attempt detected.\033[0m"
                     success=true
-
+                    break
                 elif echo "$output" | grep -q "Clock not on correctly! Restart recommended"; then
                     echo -e "\033[1;31Clock slip detected. TURF reboot required.\033[0m"
                     errorCode=100
                 elif echo "$output" | grep -q "All trigger clocks are reporting on and no LOL"; then
                     echo -e "\033[1;31No clock slips detected and system ready for RF data!\033[0m"
                     success=true
+                    break
                 elif echo "$output" | grep -zq "SURF slot#[0-9]\+ on TURFIO port#[0-9]\+ is not accessible!"; then
                     echo -e "\033[1;31mSURF not booted.\033[0m"
                     sn=$(echo "$output" | grep -oP 'slot#\K\d+' | tail -n 1)
