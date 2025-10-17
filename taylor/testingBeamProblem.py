c850d9f300fb5e372435e718275e493ab6634bbe
Taylor Coakley <coakley.64@osu.edu>
2025-10-17 11:31:05 -0400

    adding more tests

--- taylor/testingBeamProblem.py
+++ taylor/testingBeamProblem.py
@@ -7,13 +7,16 @@ dev = PueoTURF()
 tio = PueoTURFIO((dev, 0), 'TURFGTP')
 surf = PueoSURF((tio, 6), 'TURFIO')
 
+print(" ")
+print('Trying to full send upper and lower bits twice')
+print(" ")
+time.sleep(3)
+
 print('Unmask All and Wait 5s')
 surf.levelone.write(0x2008, 0x00000)
 surf.levelone.write(0x200C, 0x8000000)
 time.sleep(5) 
 dev.trig.scaler.scalers(verbose = True) 
-
-
 
 print('Mask All and Wait 5s Attempt 1')
 surf.levelone.write(0x2008, 0xFFFFF)
@@ -25,6 +28,54 @@ dev.trig.scaler.scalers(verbose = True)
 print('Mask All and Wait 5s Attempt 2')
 surf.levelone.write(0x2008, 0xFFFFF)
 surf.levelone.write(0x200C, 0xFFFFFFF)
+time.sleep(5) 
+dev.trig.scaler.scalers(verbose = True)
+
+
+print(" ")
+print('Trying to only send upper bits twice')
+print(" ")
+time.sleep(3)
+
+print('Unmask All and Wait 5s')
+surf.levelone.write(0x2008, 0x00000)
+surf.levelone.write(0x200C, 0x8000000)
+time.sleep(5) 
+dev.trig.scaler.scalers(verbose = True) 
+
+print('Mask All and Wait 5s Attempt 1')
+surf.levelone.write(0x2008, 0xFFFFF)
+surf.levelone.write(0x200C, 0xFFFFFFF)
+time.sleep(5) 
+dev.trig.scaler.scalers(verbose = True)
+
+
+print('Mask All and Wait 5s Attempt 2')
+surf.levelone.write(0x200C, 0xFFFFFFF)
+time.sleep(5) 
+dev.trig.scaler.scalers(verbose = True)
+
+
+print(" ")
+print('Trying to only send lower bits twice')
+print(" ")
+time.sleep(3)
+
+print('Unmask All and Wait 5s')
+surf.levelone.write(0x2008, 0x00000)
+surf.levelone.write(0x200C, 0x8000000)
+time.sleep(5) 
+dev.trig.scaler.scalers(verbose = True) 
+
+print('Mask All and Wait 5s Attempt 1')
+surf.levelone.write(0x2008, 0xFFFFF)
+surf.levelone.write(0x200C, 0xFFFFFFF)
+time.sleep(5) 
+dev.trig.scaler.scalers(verbose = True)
+
+
+print('Mask All and Wait 5s Attempt 2')
+surf.levelone.write(0x2008, 0xFFFFF)
 time.sleep(5) 
 dev.trig.scaler.scalers(verbose = True)
 
