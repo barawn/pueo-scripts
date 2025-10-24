292cd661a15b8842b87f8bf6c482b4804e879f01
Taylor Coakley <coakley.64@osu.edu>
2025-10-24 10:52:12 -0400

    selecting which firmware slot that you want spare edition

---
+++ taylor/fwSelect_Spare.py
@@ -0,0 +1,23 @@
+from HskSerial import HskEthernet, HskPacket
+
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--fwslot", type=int)
+hsk = HskEthernet()
+args = parser.parse_args()
+
+tio = (0, 0x48)
+surf = [ 
+        (3, 0x9c),
+        (6, 0xa3) ]
+
+for i in range(0,4): 
+    tio = tio[1]
+    hsk.send(HskPacket(tio, 'eEnable', data=[0x40, 0x40]))
+    pkt = hsk.receive()
+    for j in range(len(surf)):
+        hsk.send(HskPacket(surf[1], 'eEnable', data =f"/lib/firmware/{args.fwslot}".encode()))
+        pkt = hsk.receive()
+        hsk.send(HskPacket(surf[1], 'eRestart', data=[0]))
