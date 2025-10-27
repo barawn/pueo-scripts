386c062d6a062f5ce051c252fd88385e7ca2977c
tmcoakley <coakley.64@osu.edu>
2025-10-27 16:52:19 +0000

    works as far as setting a specific L1 goes

--- taylor/autoMask_All.py
+++ taylor/autoMask_All.py
@@ -14,33 +14,36 @@ parser.add_argument("--trigrate", type=int)
 parser.add_argument("--removesurf",  type=str)
 args = parser.parse_args()
 
-removed = list(map(int,args.removesurf.split(',')))
+if not args.removesurf:
+    removed  =[]
+else: 
+    removed = list(map(int,args.removesurf.split(',')))
 
-for j in range(len(removed)): 
-    premask |= (1 << removed[j])
 
 dev = PueoTURF()
-# need to sample which are masked off already? 
-premask = (dev.trig.mask)
+premask = dev.trig.mask
+for remove in removed:
+    premask |= (1 << remove)
 
 print(f'Before: {bin(premask)}')
 
 trigs = dev.trig.scaler.scalers()
 surfs = trigs[0:7] + trigs[8:15] + trigs[16:22] + trigs[24:30]
 maskoff = []
-for i in range(len(surfs)): 
-    if surfs[i] >= args.trigrate: 
+for i in range(len(surfs)):
+    if surfs[i] >= args.trigrate:
         print(f'SURF {i} firing too fast!')
-        print(f'Masking off!')
         maskoff.append(i)
+if maskoff: 
+    print('Masking off!')
 
 for j in range(len(maskoff)):
     premask |= (1 << maskoff[j])
 
-for k in range(27): 
-    if k not in maskoff and k not in removed: 
-        premask &= ~(1 << 7)
+for k in range(27):
+    if k not in maskoff and k not in removed:
+        premask &= ~(1 << k)
 
-print(f'After: {bin(premask)}') 
+print(f'After: {bin(premask)}')
 
-dev.trig.mask = premask 
+dev.trig.mask = premask
