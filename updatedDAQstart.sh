9ba133e119bd7393b8ab6f00f2b56c05bf586aeb
Taylor Coakley <coakley.64@osu.edu>
2025-11-30 00:48:30 +0000

    updating the DAQ startup

--- updatedDAQstart.sh
+++ updatedDAQstart.sh
@@ -1,4 +1,7 @@
-taylor/ppython checkIfReady.py
+taylor/ppython checkIfReady.py --tio 3 --slots 0,1,2,3,4,5
+taylor/ppython checkIfReady.py --tio 2 --slots 0,1,2,3,4,5
+taylor/ppython checkIfReady.py --tio 1 --slots 0,1,2,3,4,5,6
+taylor/ppython checkIfReady.py --tio 0 --slots 0,1,2,3,4,5,6
 taylor/ppython startup/turfManualStartup.py 
 taylor/ppython taylor/pingPongReq.py
 taylor/ppython startup/surfStartup.py --tio 3 --slots 0,1,2,3,4,5 --enable
@@ -19,4 +22,7 @@ taylor/ppython taylor/thresholdRFAll.py --threshold 13000 --subthreshold 11000 -
 taylor/ppython taylor/thresholdRFAll.py --threshold 13000 --subthreshold 11000 --tio 2 --slots 0,1,2,3,4,5 --unmask --nbeams 48
 taylor/ppython taylor/thresholdRFAll.py --threshold 13000 --subthreshold 11000 --tio 1 --slots 0,1,2,3,4,5,6 --unmask --nbeams 48
 taylor/ppython taylor/thresholdRFAll.py --threshold 13000 --subthreshold 11000 --tio 0 --slots 0,1,2,3,4,5,6 --unmask --nbeams 48
-taylor/ppython checkIfReady.py
+taylor/ppython checkIfReady.py --tio 3 --slots 0,1,2,3,4,5
+taylor/ppython checkIfReady.py --tio 2 --slots 0,1,2,3,4,5
+taylor/ppython checkIfReady.py --tio 1 --slots 0,1,2,3,4,5,6
+taylor/ppython checkIfReady.py --tio 0 --slots 0,1,2,3,4,5,6
