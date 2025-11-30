6b6613fe2a8f360cd5521189643ff35e78d53ff4
tmcoakley <coakley.64@osu.edu>
2025-11-30 13:37:11 +1300

    making them all slot options

--- checkIfReady.py
+++ checkIfReady.py
@@ -1,67 +1,76 @@
 import sys
+import argparse 
 from pueo.turf import PueoTURF
 from pueo.turfio import PueoTURFIO
 from pueo.surf import PueoSURF
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--tio", type=str, default="0,1,2,3", 
+        help="comma-separated list of TURFIOs to initialize")
+parser.add_argument("--slots", type=str, default="0,1,2,3,4,5,6")
 
-tio0 = (0, 0x58)
-surf0 = [ (0, 0x97),
-        (1, 0xa0),
-        (2, 0x99),
-        (3, 0x8d),
-        (4, 0x9d),
-        (5, 0x94),
-        (6, 0x8a) ]
+args = parser.parse_args()
 
-tio1 = (1, 0x50)
-surf1 = [ (0, 0x8c),
-        (1, 0x95),
-        (2, 0x9f),
-        (3, 0x9a),
-        (4, 0x87),
-        (5, 0x85), 
-        (6, 0x91)]
+slotList = list(map(int,args.slots.split(',')))
 
-tio2 = (2, 0x40)
-surf2 = [ (0, 0x89),
-        (1, 0x88),
-        (2, 0x9e),
-        (3, 0x8b),
-        (4, 0xa1),
-        (5, 0x98)]
+if args.tio == '0':
+    tios = (0, 0x58)
+    surfs = [ (0, 0x97),
+            (1, 0xa0),
+            (2, 0x99),
+            (3, 0x8d),
+            (4, 0x9d),
+            (5, 0x94),
+            (6, 0x8a) ]
+elif args.tio == '1':
+    tios = (1, 0x50)
+    surfs = [ (0, 0x8c),
+            (1, 0x95),
+            (2, 0x9f),
+            (3, 0x9a),
+            (4, 0x87),
+            (5, 0x85), 
+            (6, 0x91)]
+elif args.tio == '2':
+    tios = (2, 0x40)
+    surfs = [ (0, 0x89),
+            (1, 0x88),
+            (2, 0x9e),
+            (3, 0x8b),
+            (4, 0xa1),
+            (5, 0x98)]
+elif args.tio == '3':
+    tios = (3, 0x48)
+    surfs = [ (0, 0x93),
+            (1, 0x9b),
+            (2, 0x86),
+            (3, 0x8e),
+            (4, 0x90),
+            (5, 0x92) ]
+elif args.tio == 't':
+    tios = (3, 0x48)
+    surfs = [ (0, 0x93) ]
 
-tio3 = (3, 0x48)
-surf3 = [ (0, 0x93),
-        (1, 0x9b),
-        (2, 0x86),
-        (3, 0x8e),
-        (4, 0x90),
-        (5, 0x92) ]
 
-tios = [tio0, tio1, tio2, tio3]
-surfs = [surf0, surf1, surf2, surf3]
 dev = PueoTURF()
+ 
+tio = PueoTURFIO((dev, tios[0]), 'TURFGTP')
 
-for i in range(0,4): 
-    tio = PueoTURFIO((dev, i), 'TURFGTP')
-    print(i)
-    surfv = surfs[i] 
-    for j in range(len(surfv)):
-        val = (surfv[j][0])
-        print(j)
-        try:
-            surf = PueoSURF((tio, val), 'TURFIO')
-        except: 
-            print('RX clock off')
-            sys.exit()
-        
-        lolval = surf.lol 
-        rftrig = surf.trig_clock_en
-        if lolval == 1 or rftrig == 0: 
-            print('Clock not on correctly! Restart recommended')
-            sys.exit()
-        else: 
-            continue 
+for s in slotList:
+    val = (surfs[s][0])
+    try:
+        surf = PueoSURF((tio, val), 'TURFIO')
+    except: 
+        print('RX clock off')
+        sys.exit()
+    
+    lolval = surf.lol 
+    rftrig = surf.trig_clock_en
+    if lolval == 1 or rftrig == 0: 
+        print('Clock not on correctly! Restart recommended')
+        sys.exit()
+    else: 
+        continue 
 
 print('All trigger clocks are reporting on and no LOL')
 
